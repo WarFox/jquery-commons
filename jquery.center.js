ef0fdbd3c5b5b8129742440e96dac3e67c22c736
Deepu Mohan Puhrote <mail@deepumohan.com>
2010-12-01 07:35:25 +0000

    added placeholder parameter

--- jquery.center.js
+++ jquery.center.js
@@ -4,13 +4,15 @@
     By default it will center the element both vertically and horizontally.
     direction : both (default), horiz, vert
     onWindowResize: true, false (default)
+	placeholder: id of the placeholder. default is window
 */
 jQuery.fn.center = function (options) {
 
-    var settings = {   direction : 'both', onWindowResize: false };
+    var settings = {   direction : 'both', onWindowResize: false , placeholder : window};
     
     jQuery.extend(settings, options);
     
+	var placeholder = settings.placeholder;
     var direction = settings.direction;
     var onWindowResize = settings.onWindowResize;
     var obj = this;
@@ -18,20 +20,19 @@ jQuery.fn.center = function (options) {
     this.css("position","absolute");    
     
     if (direction == 'both') {
-        this.css("top", ( $(window).height() - this.height() ) / 2+$(window).scrollTop() + "px");
-        this.css("left", ( $(window).width() - this.width() ) / 2+$(window).scrollLeft() + "px");    
+        this.css("top", ( $(placeholder).height() - this.height() ) / 2+$(placeholder).scrollTop() + "px");
+        this.css("left", ( $(placeholder).width() - this.width() ) / 2+$(placeholder).scrollLeft() + "px");    
     } else if (direction == 'horiz') {
-        this.css("left", ( $(window).width() - this.width() ) / 2+$(window).scrollLeft() + "px");
+        this.css("left", ( $(placeholder).width() - this.width() ) / 2+$(placeholder).scrollLeft() + "px");
     } else if (direction == 'vert') {
-        this.css("top", ( $(window).height() - this.height() ) / 2+$(window).scrollTop() + "px");
+        this.css("top", ( $(placeholder).height() - this.height() ) / 2+$(placeholder).scrollTop() + "px");
     }
     
     if(onWindowResize == true) {
-        $(window).resize(function() {
+        $(placeholder).resize(function() {
             jQuery.extend(options,{ onWindowResize: false});
             $(obj).center(options);
         });        
-    }    
-        
+    }
     return this;
 };
